bdc9c1268f556701401e572d100befcf9a0fbe75
revanth <revanthaladeen7@gmail.com>
2023-11-07 11:47:05 +0530

    project work

--- JavaScript/1_howToWorkWith/script.js
+++ JavaScript/1_howToWorkWith/script.js
@@ -1,28 +1,4 @@
 let submitBtn = document.getElementById('submitBtn'),
-<<<<<<< HEAD
-    links = document.querySelectorAll('.links');
-
-submitBtn.addEventListener('click', (e) => {
-    alert('i have been clicked');
-});
-
-// links.addEventListener('mouseenter', (e) => {
-//     console.log('the mouse entered the links');
-//     // links.removeEventListener('mouseenter', mouseEnter);
-// });
-
-let h1 = document.querySelector('h1');
-
-h1.addEventListener('mouseover', (e) => {
-    h1.style.backgroundcolor = 'red';
-});
-
-document.addEventListener('keyup', (e) => {
-    console.log(e.code);
-});
-
-document.addEventListener('DOMContentLoaded', (e) => console.log('DOM Content Loaded'));
-=======
     links = document.querySelector('.links'),
     h1 = document.querySelector('h1');
 
@@ -49,4 +25,3 @@ function whatKeyPressed(event) {
 }
 
 document.addEventListener('DOMContentLoaded', (e) => console.log('dom content loaded perfectly'));
->>>>>>> 8bcddbfb6ee1bf1719ea708fcfe5117aa63b6781
